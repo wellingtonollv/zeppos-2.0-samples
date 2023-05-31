e9b652f3590e691c164d6c9cfcd923b6fdf1df21
Wellington <28570773+wellingtonollv@users.noreply.github.com>
2023-05-31 02:17:48 -0300

    :sparkles: feat: add message builder

--- showcase/app.js
+++ showcase/app.js
@@ -1,13 +1,25 @@
 import { setStatusBarVisible } from '@zos/ui'
+import './shared/device-polyfill'
+import { MessageBuilder } from './shared/message'
+import { getPackageInfo } from '@zos/app'
+import * as ble from '@zos/ble'
 
 App({
-  globalData: {},
+  globalData: {
+    messageBuilder: null,
+  },
   onCreate(options) {
     console.log('app on create invoke')
-    setStatusBarVisible(false)
+    setStatusBarVisible(false);
+    console.log('app on create invoke')
+    const { appId } = getPackageInfo()
+    const messageBuilder = new MessageBuilder({ appId, appDevicePort: 20, appSidePort: 0, ble })
+    this.globalData.messageBuilder = messageBuilder
+    messageBuilder.connect()
   },
 
   onDestroy(options) {
     console.log('app on destroy invoke')
+    this.globalData.messageBuilder && this.globalData.messageBuilder.disConnect()
   }
 })
